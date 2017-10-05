1435ea6ac2dc5eaeba2f31944cc2b1ae539b4298
Hoang Dao <daohm@mail.uc.edu>
2017-10-05 13:49:19 -0400

    no change

--- draft.py
+++ draft.py
@@ -1,2 +1,15 @@
-a = 'Hoang'
-print(a.split('a'))
+def isValid(s):
+    stack = []
+    d = {"]":"[", "}":"{", ")":"("}
+    # print(d[")"])
+    for char in s:
+        if char in d.values():
+            stack.append(char)
+        elif char in d.keys():
+            if not stack or d[char] != stack.pop():
+                return False
+        else:
+            return False
+    return stack == []
+
+print(isValid("()"))
